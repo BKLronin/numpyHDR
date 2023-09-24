accaf550912b5c3b3947a4f9ffcb6b4ae99938d7
Thomas <therrmann23@gmail.com>
2023-09-24 09:57:24 +0200

    - Added safety measure to round incoming Exposure to int before processing.

--- picamburst.py
+++ picamburst.py
@@ -13,9 +13,14 @@ picam2.set_controls({"AeEnable": 1})
 picam2.set_controls({"AfMode": controls.AfModeEnum.Manual })
 picam2.set_controls({"LensPosition": 0.0 })
 
-def get_exposure_stack(factor: int = 2):
-    '''Returns a list with arrays that contain different exposures controlled by the factor.'''
-    '''The Autoamtically set exposure of the first frame is saved and multiplied or divided by the factor to get the above or under epxosures.'''
+def get_exposure_stack(factor: int = 2) -> list:
+    '''Returns a list with arrays that contain different exposures controlled by the factor.
+    The Autoamtically set exposure of the first frame is saved and multiplied or divided by the factor to get the above or under epxosures.
+
+    :param factor: The EV difference in the exposure of the desired images
+    :return: List containing arrays with the pictures
+    '''
+
 
     picam2.start()
     time.sleep(1)
